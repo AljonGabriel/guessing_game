0962b1ac559ea58f2f7bb7d9ccf171e9bf0597d5
Aljonator <121176500+AljonGabriel@users.noreply.github.com>
2023-06-07 14:57:05 +0800

    first commit

---
+++ apps.js
@@ -0,0 +1,500 @@
+const cardsArray = {
+  easy:  [
+    {
+      name: "Cheese Burger",
+      location: "images/cheeseburger.png",
+    },
+    {
+      name: "Fries",
+      location: "images/fries.png",
+    },
+    {
+      name: "Hotdog",
+      location: "images/hotdog.png",
+    },
+    {
+      name: "Ice Cream",
+      location: "images/ice-cream.png",
+    },
+    {
+      name: "Milk Shake",
+      location: "images/milkshake.png",
+    },
+    {
+      name: "Pizza",
+      location: "images/pizza.png",
+    },
+    {
+      name: "Cheese Burger",
+      location: "images/cheeseburger.png",
+    },
+    {
+      name: "Fries",
+      location: "images/fries.png",
+    },
+    {
+      name: "Hotdog",
+      location: "images/hotdog.png",
+    },
+    {
+      name: "Ice Cream",
+      location: "images/ice-cream.png",
+    },
+    {
+      name: "Milk Shake",
+      location: "images/milkshake.png",
+    },
+    {
+      name: "Pizza",
+      location: "images/pizza.png",
+    },
+  ],
+
+  normal: [
+    {
+      name: "Cheese Burger",
+      location: "images/cheeseburger.png",
+    },
+    {
+      name: "Fries",
+      location: "images/fries.png",
+    },
+    {
+      name: "Hotdog",
+      location: "images/hotdog.png",
+    },
+    {
+      name: "Ice Cream",
+      location: "images/ice-cream.png",
+    },
+    {
+      name: "Milk Shake",
+      location: "images/milkshake.png",
+    },
+    {
+      name: "Pizza",
+      location: "images/pizza.png",
+    },
+    {
+      name: "Cheese Burger",
+      location: "images/cheeseburger.png",
+    },
+    {
+      name: "Fries",
+      location: "images/fries.png",
+    },
+    {
+      name: "Hotdog",
+      location: "images/hotdog.png",
+    },
+    {
+      name: "Ice Cream",
+      location: "images/ice-cream.png",
+    },
+    {
+      name: "Milk Shake",
+      location: "images/milkshake.png",
+    },
+    {
+      name: "Pizza",
+      location: "images/pizza.png",
+    },
+    {
+      name: "Cheese Burger",
+      location: "images/cheeseburger.png",
+    },
+    {
+      name: "Fries",
+      location: "images/fries.png",
+    },
+    {
+      name: "Hotdog",
+      location: "images/hotdog.png",
+    },
+    {
+      name: "Ice Cream",
+      location: "images/ice-cream.png",
+    },
+    {
+      name: "Milk Shake",
+      location: "images/milkshake.png",
+    },
+    {
+      name: "Pizza",
+      location: "images/pizza.png",
+    },
+    {
+      name: "Cheese Burger",
+      location: "images/cheeseburger.png",
+    },
+    {
+      name: "Fries",
+      location: "images/fries.png",
+    },
+    {
+      name: "Hotdog",
+      location: "images/hotdog.png",
+    },
+    {
+      name: "Ice Cream",
+      location: "images/ice-cream.png",
+    },
+    {
+      name: "Milk Shake",
+      location: "images/milkshake.png",
+    },
+    {
+      name: "Pizza",
+      location: "images/pizza.png",
+    },
+  ],
+
+  hard: [
+    {
+      name: "Cheese Burger",
+      location: "images/cheeseburger.png",
+    },
+    {
+      name: "Fries",
+      location: "images/fries.png",
+    },
+    {
+      name: "Hotdog",
+      location: "images/hotdog.png",
+    },
+    {
+      name: "Ice Cream",
+      location: "images/ice-cream.png",
+    },
+    {
+      name: "Milk Shake",
+      location: "images/milkshake.png",
+    },
+    {
+      name: "Pizza",
+      location: "images/pizza.png",
+    },
+    {
+      name: "Cheese Burger",
+      location: "images/cheeseburger.png",
+    },
+    {
+      name: "Fries",
+      location: "images/fries.png",
+    },
+    {
+      name: "Hotdog",
+      location: "images/hotdog.png",
+    },
+    {
+      name: "Ice Cream",
+      location: "images/ice-cream.png",
+    },
+    {
+      name: "Milk Shake",
+      location: "images/milkshake.png",
+    },
+    {
+      name: "Pizza",
+      location: "images/pizza.png",
+    },
+    {
+      name: "Cheese Burger",
+      location: "images/cheeseburger.png",
+    },
+    {
+      name: "Fries",
+      location: "images/fries.png",
+    },
+    {
+      name: "Hotdog",
+      location: "images/hotdog.png",
+    },
+    {
+      name: "Ice Cream",
+      location: "images/ice-cream.png",
+    },
+    {
+      name: "Milk Shake",
+      location: "images/milkshake.png",
+    },
+    {
+      name: "Pizza",
+      location: "images/pizza.png",
+    },
+    {
+      name: "Cheese Burger",
+      location: "images/cheeseburger.png",
+    },
+    {
+      name: "Fries",
+      location: "images/fries.png",
+    },
+    {
+      name: "Hotdog",
+      location: "images/hotdog.png",
+    },
+    {
+      name: "Ice Cream",
+      location: "images/ice-cream.png",
+    },
+    {
+      name: "Milk Shake",
+      location: "images/milkshake.png",
+    },
+    {
+      name: "Pizza",
+      location: "images/pizza.png",
+    },
+    {
+      name: "Cheese Burger",
+      location: "images/cheeseburger.png",
+    },
+    {
+      name: "Fries",
+      location: "images/fries.png",
+    },
+    {
+      name: "Hotdog",
+      location: "images/hotdog.png",
+    },
+    {
+      name: "Ice Cream",
+      location: "images/ice-cream.png",
+    },
+    {
+      name: "Milk Shake",
+      location: "images/milkshake.png",
+    },
+    {
+      name: "Pizza",
+      location: "images/pizza.png",
+    },
+    {
+      name: "Cheese Burger",
+      location: "images/cheeseburger.png",
+    },
+    {
+      name: "Fries",
+      location: "images/fries.png",
+    },
+    {
+      name: "Hotdog",
+      location: "images/hotdog.png",
+    },
+    {
+      name: "Ice Cream",
+      location: "images/ice-cream.png",
+    },
+    {
+      name: "Milk Shake",
+      location: "images/milkshake.png",
+    },
+    {
+      name: "Pizza",
+      location: "images/pizza.png",
+    },
+    {
+      name: "Cheese Burger",
+      location: "images/cheeseburger.png",
+    },
+    {
+      name: "Fries",
+      location: "images/fries.png",
+    },
+    {
+      name: "Hotdog",
+      location: "images/hotdog.png",
+    },
+    {
+      name: "Ice Cream",
+      location: "images/ice-cream.png",
+    },
+    {
+      name: "Milk Shake",
+      location: "images/milkshake.png",
+    },
+    {
+      name: "Pizza",
+      location: "images/pizza.png",
+    },
+  ],
+};
+
+
+let chosenCards = [];
+let chosenCardsId = [];
+let matchFound = [];
+let timeLeft;
+let gameStarted = false;
+
+const grid = document.querySelector("#grid");
+const score = document.querySelector("#score");
+
+const inputPlayerName = document.querySelector("#enter-name");
+const playerName = document.querySelector("#player-name");
+const startGame = document.querySelector('button[type="submit"]');
+const displayTime = document.getElementById("time");
+const resultDisplay = document.getElementById("result");
+const moveDisplay = document.getElementById("moveInfoDisplay");
+const playAgain = document.getElementById("play-again");
+
+const playerNameElement = document.querySelector('.player-name')
+const scoreElement = document.querySelector('.player-score')
+const timeElement = document.querySelector('.player-time')
+
+const levels = {
+  easyLevel: document.getElementById("level-easy"),
+  normalLevel: document.getElementById("level-normal"),
+  hardLevel: document.getElementById("level-hard"),
+};
+
+playAgain.style.display = "none";
+playerNameElement.style.display = "none"
+scoreElement.style.display = "none"
+timeElement.style.display = "none"
+
+
+startGame.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  const trimPlayerName = inputPlayerName.value.trim();
+  const selectedLevel = getSelectedLevel();
+  console.log(gameStarted)
+  
+
+  grid.classList.remove('hidden');
+
+  if (selectedLevel === "easy") {
+    timeLeft = 50;
+  } else if (selectedLevel === "normal") {
+    timeLeft = 55;
+  } else {
+    timeLeft = 80;
+  }
+
+  if (trimPlayerName && selectedLevel) {
+
+    
+    playerName.innerHTML = trimPlayerName;
+    inputPlayerName.value = "";
+    startGame.style.display = "none";
+
+    levels.easyLevel.disabled = true; // Disable level selection after starting the game
+    levels.normalLevel.disabled = true;
+    levels.hardLevel.disabled = true;
+    inputPlayerName.disabled = true;
+
+    playerNameElement.style.display = "inline"
+    scoreElement.style.display = "inline"
+    timeElement.style.display = "inline"
+  
+
+    countDownTimer = setInterval(countDown, 500);
+    board(selectedLevel);
+  } else {
+    alert("Please enter name and select a level to start the game");
+    
+  }
+});
+
+
+
+
+function getSelectedLevel() {
+  if (levels.easyLevel.checked) {
+    return "easy";
+  } else if (levels.normalLevel.checked) {
+    return "normal";
+  } else if (levels.hardLevel.checked) {
+    return "hard";
+  } else {
+    return null; // No level selected
+  }
+}
+
+function board(selectedLevel) {
+  const cards = cardsArray[selectedLevel]; // Retrieve the cards array for the selected level
+  const shuffledCards = cards.sort(() => 0.5 - Math.random()); // Randomize the order of cards
+
+  for (let i = 0; i < shuffledCards.length; i++) {
+    const createImg = document.createElement("img");
+    createImg.setAttribute("data-id", i);
+    createImg.setAttribute("src", "images/blank.png");
+    createImg.addEventListener("click", flipCards);
+    grid.append(createImg);
+  }
+}
+
+function checkMatch(selectedLevel) {
+  const cards = document.querySelectorAll("img");
+
+  if (chosenCardsId[0] === chosenCardsId[1]) {
+    moveDisplay.innerHTML = "You click the same Card Idiot!";
+    cards[chosenCardsId[0]].setAttribute("src", "images/blank.png");
+    cards[chosenCardsId[1]].setAttribute("src", "images/blank.png");
+  } else if (chosenCards[0] === chosenCards[1]) {
+    moveDisplay.innerHTML = "Finally you found a match after along time !!!";
+    cards[chosenCardsId[0]].removeEventListener("click", flipCards);
+    cards[chosenCardsId[1]].removeEventListener("click", flipCards);
+    cards[chosenCardsId[0]].classList.add("found");
+    cards[chosenCardsId[1]].classList.add("found");
+    matchFound.push(chosenCards);
+    console.log(matchFound);
+  } else if (chosenCards[0] !== chosenCards[1]) {
+    moveDisplay.innerHTML = "BAKA !!!! Wrong match muscle head!!!";
+    cards[chosenCardsId[0]].setAttribute("src", "images/blank.png");
+    cards[chosenCardsId[1]].setAttribute("src", "images/blank.png");
+  }
+
+  console.log(chosenCards);
+
+  chosenCards = [];
+  chosenCardsId = [];
+
+  score.innerHTML = matchFound.length;
+
+  if (matchFound.length === cardsArray[selectedLevel].length / 2) {
+    clearInterval(countDownTimer);
+    resultDisplay.innerHTML = "Your score: " + matchFound.length;
+    score.innerHTML = "You won !";
+    playAgain.style.display = "block";
+    moveDisplay.innerText = " "
+  }
+}
+
+function flipCards() {
+  const cardsId = this.getAttribute("data-id");
+  const selectedLevel = getSelectedLevel();
+
+  if (selectedLevel && cardsArray[selectedLevel]) {
+    const cards = cardsArray[selectedLevel];
+    chosenCards.push(cards[cardsId].name);
+    chosenCardsId.push(cardsId);
+    console.log(chosenCards, chosenCardsId);
+    this.setAttribute("src", cards[cardsId].location);
+
+    if (chosenCards.length === 2) {
+      setTimeout(() => checkMatch(selectedLevel), 200);
+    }
+  }
+}
+
+function countDown() {
+  timeLeft--;
+  displayTime.innerHTML = timeLeft;
+
+  if (timeLeft === 0) {
+    clearInterval(countDownTimer);
+    resultDisplay.innerHTML = "Your score :" + matchFound.length;
+    moveDisplay.style.display = "none";
+    playAgain.style.display = "block";
+    score.innerHTML = "You lose !"
+
+    const cards = document.querySelectorAll("img");
+    cards.forEach((card) => {
+      card.removeEventListener("click", flipCards);
+      card.style.filter = "grayscale(100%)"; // Apply grayscale filter to the card image
+    });
+  }
+}
